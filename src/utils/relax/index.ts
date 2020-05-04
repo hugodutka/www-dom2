f644d9606d8643573f8ec1ca2feb8c8d57ef50e7
Hugo Dutka <contact@hugodutka.com>
2020-05-04 13:54:42 +0200

    fix case

--- src/utils/relax/index.ts
+++ src/utils/relax/index.ts
@@ -3,7 +3,7 @@
 
 import { Action, Reducer, State, Store, combineReducers } from './store'
 import { Renderer, Component, PropMap, connect } from './renderer'
-import { JSX } from './JSX'
+import { JSX } from './jsx'
 
 export {
   Action, Reducer, State, Store, combineReducers, Renderer, Component, PropMap, connect, JSX
