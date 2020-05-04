c28584bb1d60885d14b491c2ccdb257d46b69349
Hugo Dutka <contact@hugodutka.com>
2020-05-04 13:51:34 +0200

    export combineReducers from relax

--- src/utils/relax/index.ts
+++ src/utils/relax/index.ts
@@ -1,10 +1,10 @@
 // This module provides state management and rendering utilities. The name is a pun on React and
 // Redux.
 
-import { Action, Reducer, State, Store } from './store'
+import { Action, Reducer, State, Store, combineReducers } from './store'
 import { Renderer, Component, PropMap, connect } from './renderer'
 import { JSX } from './JSX'
 
 export {
-  Action, Reducer, State, Store, Renderer, Component, PropMap, connect, JSX
+  Action, Reducer, State, Store, combineReducers, Renderer, Component, PropMap, connect, JSX
 }
